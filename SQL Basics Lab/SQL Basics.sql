dce0c5b575f0da8dafce160aaf470e39d8ddfa1e
luijkr <luijkr>
2025-02-10 15:33:58 +0000

    add exercises

--- SQL Basics Lab/SQL Basics.sql
+++ SQL Basics Lab/SQL Basics.sql
@@ -4,13 +4,19 @@ SELECT
 	*
 FROM samples.nyctaxi.trips;
 
-SELECT * FROM my_catalog.nyctaxi.trips LIMIT 10;
+SELECT *
+FROM my_catalog.nyctaxi.trips
+LIMIT 10;
 
-SELECT * FROM my_catalog.nyctaxi.trips WHERE fare_amount > 5 LIMIT 10;
+SELECT *
+FROM my_catalog.nyctaxi.trips
+WHERE fare_amount > 5
+LIMIT 10;
 
 INSERT INTO my_catalog.nyctaxi.trips 
-VALUES 
-(99999, '2025-02-10 08:00:00', '2025-02-10 08:30:00', 5.2, 20.5, 10001, 10002);
+VALUES (
+	99999, '2025-02-10 08:00:00', '2025-02-10 08:30:00', 5.2, 20.5, 10001, 10002
+);
 
 UPDATE my_catalog.nyctaxi.trips
 SET fare_amount = 25.0
@@ -18,3 +24,11 @@ WHERE trip_id = 99999;
 
 DELETE FROM my_catalog.nyctaxi.trips
 WHERE trip_id = 99999;
+
+-- EXERCISE
+-- Update all values for `fare_amount` where the pickup zipcode equals `10023`
+UPDATE ...
+
+-- EXERCISE
+-- Cleanup: delete the table you just created
+-- Hint: use the `DROP TABLE` command
